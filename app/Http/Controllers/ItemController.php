93be46e21b3df3784a2f84f22ca5d7f7ee8c14ee
idn-fahmifn <fahmifn.idn@gmail.com>
2026-01-14 10:49:28 +0700

    3.3

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use App\Models\Item;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 
@@ -13,8 +14,10 @@ class ItemController extends Controller
     {
         $data = Item::all();
         $category = Category::all();
-        return view('item.index', 
-        compact('data', 'category'));
+        return view(
+            'item.index',
+            compact('data', 'category')
+        );
     }
 
     public function store(Request $request)
@@ -36,9 +39,22 @@ class ItemController extends Controller
         ];
 
         // kondisi saat upload file
+        if ($request->hasFile('image')) {
 
-        Item::create($simpan); //menambahkan data ke database
-        return redirect()->route('category.index')->with('success', 'Item Created');
+            $gambar = $request->file('image'); //mengambil data file yang diupload oleh user
+            $path = 'public/images/items'; //path penyimpanan
+            $ext = $gambar->getClientOriginalExtension();
+            $nama = 'item-image-' . Carbon::now('Asia/Jakarta')->format('Ymdhis') . $ext;
+
+            // nilai yang akan disimpan ke database : 
+            $simpan['image'] = $nama;
+
+        }
+
+        return $simpan;
+
+        // Item::create($simpan); //menambahkan data ke database
+        // return redirect()->route('category.index')->with('success', 'Item Created');
     }
 
     public function detail($param)
